d6efe80ae77f32a8c7bfcd91d78a9da1579cc496
gitbabis <46954646+gitbabis@users.noreply.github.com>
2026-01-13 12:55:00 +0200

    Update WiFi and MQTT credentials in secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -2,14 +2,14 @@
 #define SECRETS_H
 
 // WiFi Credentials
-const char *ssid = "babis";
-const char *password = "babishome!";
+const char *ssid = "ΤΟ_WIFI_ΣΑΣ";
+const char *password = "ΤΟ_PASSWORD_ΣΑΣ";
 
 // MQTT Broker Details
 const char *mqtt_server = "hass.syros.aegean.gr";
 const int mqtt_port = 1883;
-const char *mqtt_user = "dpsd26000";      // Leave blank if not needed
-const char *mqtt_password = "dpsd26000!"; // Leave blank if not needed
+const char *mqtt_user = "mqtt_username";      // Leave blank if not needed
+const char *mqtt_password = "mqtt_password"; // Leave blank if not needed
 
 // MQTT Topic
 const char *mqtt_topic = "homeassistant/sensor/uno_r4/state";
